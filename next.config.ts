9a510ffd540f710b4cd0fe723964b4b545a2e797
Soner AYGÖRMEZ <aygormez@gmail.com>
2026-02-09 16:32:47 +0300

    fix: use absolute path for next-intl config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+import path from "path";
+const withNextIntl = createNextIntlPlugin(path.resolve("./src/i18n/request.ts"));
 
 const nextConfig: NextConfig = {
     // Image optimization
